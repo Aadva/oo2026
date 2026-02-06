3b87221f71c45c9a7356f863bf7ebf0f7632e893
Mirko Aadva <maadva@tlu.ee>
2026-02-06 13:43:02 +0200

    controller, entity, repository, andmebaasiga ühendus

---
+++ veebipood/src/main/java/ee/mirko/veebipood/controller/ProductController.java
@@ -0,0 +1,39 @@
+package ee.mirko.veebipood.controller;
+
+import ee.mirko.veebipood.entity.Product;
+import ee.mirko.veebipood.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class ProductController {
+
+    // localhost:8080/products
+//    @GetMapping("products")
+//    public String helloworld(){
+//        return "Hello World!";
+//    }
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping("products")
+    public List<Product> getProducts(){
+        return productRepository.findAll();
+    }
+
+    @DeleteMapping("products/{id}")
+    public List<Product> deleteProduct(@PathVariable Long id){
+        productRepository.deleteById(id); // kustutan
+        return productRepository.findAll(); // uuenenud seis
+    }
+
+    @PostMapping("products")
+    public List<Product> addProduct(@RequestBody Product product){
+        productRepository.save(product); // siin salvestab
+        return productRepository.findAll(); // siin on uuenenud seis
+    }
+
+}
---
+++ veebipood/src/main/java/ee/mirko/veebipood/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package ee.mirko.veebipood.repository;
+
+import ee.mirko.veebipood.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// repository --> andmebaasiga suhtlemiseks. tema sees on kõik funktsioonid, mida on võimalik andmebaasiga teha
+
+public interface ProductRepository  extends JpaRepository<Product,Long> {
+}
